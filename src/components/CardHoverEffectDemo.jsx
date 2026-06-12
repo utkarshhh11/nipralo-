d6be6d919833956f9a2ed8a91c058cd0f2c453ce
Utkarsh Kadam <utkarshkadam17@gmail.com>
2026-06-12 11:39:20 +0530

    card image bug

--- src/components/CardHoverEffectDemo.jsx
+++ src/components/CardHoverEffectDemo.jsx
@@ -9,64 +9,53 @@ export function CardHoverEffectDemo() {
 }
 export const projects = [
   {
-    image: "src/assets/card1.png",
+    image: "https://static.vecteezy.com/system/resources/thumbnails/014/414/700/small_2x/emirates-new-logo-on-transparent-background-free-vector.jpg",
     description:
       "Top Cargo Agents, 2016/17 Top Cargo Agents, 2005/06",
     
   },
   {
-    image: "src/assets/card2.png",
+    image: "https://1000logos.net/wp-content/uploads/2017/09/Delta-Air-Lines-Logo-1966.png",
     description:
       "Top Revenue Performance, 2002",
    
   },
   {
-    image: "src/assets/card3.png",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOFft7osc_3zJYc7xp34Vwk5PH14z50RG9-g&s",
     description:
       "Mega Tonners, 2006/07",
    
   },
   {
-    image: "src/assets/card4.png",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjWE2VGFmiT22eXLXRQD8HwofTpp0cQxukqKWZLpWKJQ&s=10",
     description:
       "CONCOR Exim Star, 2003/04.",
     
   },
-  {
-    image: "src/assets/card1.png",
+   {
+    image: "https://static.vecteezy.com/system/resources/thumbnails/014/414/700/small_2x/emirates-new-logo-on-transparent-background-free-vector.jpg",
     description:
       "Top Cargo Agents, 2016/17 Top Cargo Agents, 2005/06",
     
   },
   {
-    image: "src/assets/card2.png",
+    image: "https://1000logos.net/wp-content/uploads/2017/09/Delta-Air-Lines-Logo-1966.png",
     description:
       "Top Revenue Performance, 2002",
    
   },
   {
-    image: "src/assets/card3.png",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOFft7osc_3zJYc7xp34Vwk5PH14z50RG9-g&s",
     description:
       "Mega Tonners, 2006/07",
    
   },
   {
-    image: "src/assets/card4.png",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjWE2VGFmiT22eXLXRQD8HwofTpp0cQxukqKWZLpWKJQ&s=10",
     description:
       "CONCOR Exim Star, 2003/04.",
     
   },
-  {
-    image: "src/assets/card1.png",
-    description:
-      "Top Cargo Agents, 2016/17 Top Cargo Agents, 2005/06",
-    
-  },
-  {
-    image: "src/assets/card2.png",
-    description:
-      "Top Revenue Performance, 2002",
-   
-  },
+  
   
 ];
